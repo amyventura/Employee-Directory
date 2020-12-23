3cfdeb99957528a50be1a056be907c1268b3b5c1
Amy Ventura <amyvntr@yahoo.com>
2020-12-23 15:59:09 -0500

    added col componment

--- employee-dir/src/components/Users.js
+++ employee-dir/src/components/Users.js
@@ -23,4 +23,12 @@ function Users(props) {
     
 }
 
+{/* <ul className="list-group">
+{props.groceries.map(item => (
+  <li className="list-group-item" key={item.id}>
+    {item.name}
+  </li>
+))}
+</ul> */}
+
 export default Users;
