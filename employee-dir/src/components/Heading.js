7b20da5bccb421b581a72194f76d0552f6396332
Amy Ventura <amyvntr@yahoo.com>
2020-12-23 14:59:39 -0500

    added heading component

---
+++ employee-dir/src/components/Heading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Heading() {
+    return(
+        <div>
+            <h4>Name, Technologies, DOB, Email</h4>
+        </div>
+    )
+};
+
+export default Heading;
