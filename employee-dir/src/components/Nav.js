64b4a36141735980bb0f287f184cbe5dc95f39ae
Amy Ventura <amyvntr@yahoo.com>
2020-12-23 16:02:37 -0500

    changed nav

--- employee-dir/src/components/Nav.js
+++ employee-dir/src/components/Nav.js
@@ -5,9 +5,10 @@ function Nav() {
     return (
     <div className="jumbotron">
         <h1>Employee Directory</h1>
-        <h6>Search using the input search form</h6>
+        <h6>Click on cards to filter by heading or use the search box to narrow your results</h6>
     </div>
     )
 };
 
 export default Nav;
+
